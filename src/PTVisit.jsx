dfe43ed690cdd32a587e75232009fc29c3d7f5e1
taeoldest <alextaeyounglee@gmail.com>
2023-07-27 13:01:49 -0500

    pt visit page

---
+++ src/PTVisit.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+function PTVisit(){
+
+    return (
+        
+        <div>
+            <h1>Physical Therapy</h1>
+            <p>We would like you to have a virtual visit 
+                with a  physical therapist.</p>
+
+            
+            <a href="https://www.example.com">Schedule an Appointment</a>
+        </div>
+    )
+}
+
+export default PTVisit;
