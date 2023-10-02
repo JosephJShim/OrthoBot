086d753b782ce48cdf96198afc73819ca2c4dc29
JosephJShim <josephjshim@gmail.com>
2023-10-01 21:42:33 -0500

    2nd round of changes
    Changed to Front facing Higher Definition image, finished all the body parts

--- orthobot_frontend/src/components/BodyPartSelection.jsx
+++ orthobot_frontend/src/components/BodyPartSelection.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Title } from "./utilities/Title.jsx"
 import { Text } from "./utilities/Text.jsx"
 import { Button } from "./utilities/Button.jsx"
-import background from "./bodybackground.jpeg"
+import background from "./bodyBackground.jpg"
 
 
 export function BodyPartSelection() {
@@ -11,9 +11,9 @@ export function BodyPartSelection() {
 
     const containerStyles = {
         position: 'relative',
-        // Original Image Dimension: 3902 x 3902 px
-        width: '800px', // Adjust the container size according to your image
-        height: '800px', // Adjust the container size according to your image
+        // Original Image Dimension: 1000 x 2400 px
+        width: '1000px', // Adjust the container size according to your image
+        height: '2400px', // Adjust the container size according to your image
         backgroundImage: `url(${background})`,
         backgroundRepeat: `no-repeat`,
         backgroundSize: 'cover',
@@ -49,26 +49,11 @@ export function BodyPartSelection() {
                     style={{
                         ...buttonStyles,
                         // Front; Left Hand
-                        // Topleft: 346, 2100; Bottomright: 660, 2360
-                        left: '71px',
-                        top: '431px', 
-                        width: '64px',
-                        height: '53px',
-                    }}
-                    onClick={() => navigate("/questions/hand/0")}
-                >
-                    Hand
-                </button>
-
-                <button
-                    style={{
-                        ...buttonStyles,
-                        // Back; Left Hand
-                        // Topleft: 2030, 2100; Bottomright: 2325, 2360
-                        left: '416px',
-                        top: '431px', 
-                        width: '64px',
-                        height: '53px',
+                        // Topleft: 50, 1250; Bottomright: 230, 1500
+                        left: '50px',
+                        top: '1250px', 
+                        width: '180px',
+                        height: '250px',
                     }}
                     onClick={() => navigate("/questions/hand/0")}
                 >
@@ -79,26 +64,11 @@ export function BodyPartSelection() {
                     style={{
                         ...buttonStyles,
                         // Front; Right Hand
-                        // Topleft: 1600, 2100; BottomRight: 1900, 2360
-                        left: '328px',
-                        top: '431px', 
-                        width: '61px',
-                        height: '53px',
-                    }}
-                    onClick={() => navigate("/questions/hand/0")}
-                >
-                    Hand
-                </button>
-
-                <button
-                    style={{
-                        ...buttonStyles,
-                        // Front; Right Hand
-                        // Topleft: 3270, 2100; BottomRight: 3567, 2360
-                        left: '670px',
-                        top: '431px', 
-                        width: '61px',
-                        height: '53px',
+                        // Topleft: 775, 1250; BottomRight: 975, 1550
+                        left: '775px',
+                        top: '1250px', 
+                        width: '200px',
+                        height: '250px',
                     }}
                     onClick={() => navigate("/questions/hand/0")}
                 >
@@ -109,26 +79,11 @@ export function BodyPartSelection() {
                     style={{
                         ...buttonStyles,
                         // Front; Left Wrist
-                        // Topleft: 475, 1900; BottomRight: 660, 2050
-                        left: '97px',
-                        top: '390px', 
-                        width: '38px',
-                        height: '30px',
-                    }}
-                    onClick={() => navigate("/questions/wrist/0")}
-                >
-                    Wrist
-                </button>
-
-                <button
-                    style={{
-                        ...buttonStyles,
-                        // Back; Left Wrist
-                        // Topleft: 2150, 1900; BottomRight: 2335, 2050
-                        left: '441px',
-                        top: '390px', 
-                        width: '38px',
-                        height: '30px',
+                        // Topleft: 125, 1150; BottomRight: 275, 1250
+                        left: '125px',
+                        top: '1150px', 
+                        width: '150px',
+                        height: '100px',
                     }}
                     onClick={() => navigate("/questions/wrist/0")}
                 >
@@ -139,26 +94,11 @@ export function BodyPartSelection() {
                     style={{
                         ...buttonStyles,
                         // Front; Right Wrist
-                        // Topleft: 1560, 1900; BottomRight: 1800, 2050
-                        left: '320px',
-                        top: '390px', 
-                        width: '49px',
-                        height: '30px',
-                    }}
-                    onClick={() => navigate("/questions/wrist/0")}
-                >
-                    Wrist
-                </button>
-
-                <button
-                    style={{
-                        ...buttonStyles,
-                        // Back; Right Wrist
-                        // Topleft: 3215, 1900; BottomRight: 3454, 2050
-                        left: '659px',
-                        top: '390px', 
-                        width: '49px',
-                        height: '30px',
+                        // Topleft: 750, 1150; BottomRight: 890, 1250
+                        left: '750px',
+                        top: '1150px', 
+                        width: '240px',
+                        height: '100px',
                     }}
                     onClick={() => navigate("/questions/wrist/0")}
                 >
@@ -169,57 +109,26 @@ export function BodyPartSelection() {
                     style={{
                         ...buttonStyles,
                         // Front; Left Elbow
-                        // TopLeft: 512, 1400; BottomRight: 770, 1800
-                        left: '105px',
-                        top: '287px', 
-                        width: '53px',
-                        height: '82px',
+                        // TopLeft: 130, 850; BottomRight: 315, 1050
+                        left: '130px',
+                        top: '850px', 
+                        width: '185px',
+                        height: '200px',
                     }}
                     onClick={() => navigate("/questions/elbow/0")}
                 >
                     Elbow
                 </button>
-
-                <button
-                    style={{
-                        ...buttonStyles,
-                        // Back; Left Elbow
-                        // TopLeft: 2180, 1400; BottomRight: 2438, 1800
-                        left: '447px',
-                        top: '287px', 
-                        width: '53px',
-                        height: '82px',
-                    }}
-                    onClick={() => navigate("/questions/elbow/0")}
-                >
-                    Elbow
-                </button>
-
 
                 <button
                     style={{
                         ...buttonStyles,
                         // Front; Right Elbow
-                        // TopLeft: 1450, 1400; BottomRight: 1700, 1800
-                        left: '297px',
-                        top: '287px', 
-                        width: '53px',
-                        height: '82px',
-                    }}
-                    onClick={() => navigate("/questions/elbow/0")}
-                >
-                    Elbow
-                </button>
-
-                <button
-                    style={{
-                        ...buttonStyles,
-                        // Back; Right Elbow
-                        // TopLeft: 3130, 1400; BottomRight: 3388, 1800
-                        left: '642px',
-                        top: '287px', 
-                        width: '53px',
-                        height: '82px',
+                        // TopLeft: 700, 850; BottomRight: 870, 1050
+                        left: '700px',
+                        top: '850px', 
+                        width: '170px',
+                        height: '200px',
                     }}
                     onClick={() => navigate("/questions/elbow/0")}
                 >
@@ -230,26 +139,11 @@ export function BodyPartSelection() {
                     style={{
                         ...buttonStyles,
                         // Front; Left Shoulder
-                        // TopLeft: 600, 850; BottomRight: 900, 1120  
-                        left: '123px',
-                        top: '174px', 
-                        width: '61px',
-                        height: '55px',
-                    }}
-                    onClick={() => navigate("/questions/shoulder/0")}
-                >
-                    Shoulder
-                </button>
-
-                <button
-                    style={{
-                        ...buttonStyles,
-                        // Back; Left Shoulder
-                        // TopLeft: 2270, 850; BottomRight: 2582, 1120  
-                        left: '465px',
-                        top: '174px', 
-                        width: '61px',
-                        height: '55px',
+                        // TopLeft: 190, 515; BottomRight: 375, 675  
+                        left: '190px',
+                        top: '515px', 
+                        width: '185px',
+                        height: '160px',
                     }}
                     onClick={() => navigate("/questions/shoulder/0")}
                 >
@@ -260,11 +154,11 @@ export function BodyPartSelection() {
                     style={{
                         ...buttonStyles,
                         // Front; Right Shoulder
-                        // TopLeft: 1330, 850; BottomRight: 900, 1120  
-                        left: '273px', 
-                        top: '174px',
-                        width: '61px',
-                        height: '55px',
+                        // TopLeft: 650, 515; BottomRight: 825, 675  
+                        left: '650px', 
+                        top: '515px',
+                        width: '175px',
+                        height: '160px',
                     }}
                     onClick={() => navigate("/questions/shoulder/0")}
                 >
@@ -274,22 +168,96 @@ export function BodyPartSelection() {
                 <button
                     style={{
                         ...buttonStyles,
-                        // Front; Right Shoulder
-                        // TopLeft: 3050, 850; BottomRight: 3347, 1120  
-                        left: '625px', 
-                        top: '174px',
-                        width: '61px',
-                        height: '55px',
+                        // Front; Hip
+                        // TopLeft: 300, 1130; BottomRight: 715, 1300  
+                        left: '300px', 
+                        top: '1130px',
+                        width: '415px',
+                        height: '170px',
                     }}
-                    onClick={() => navigate("/questions/shoulder/0")}
+                    onClick={() => navigate("/questions/hip/0")}
                 >
-                    Shoulder
+                    Hip
+                </button>
+
+                <button
+                    style={{
+                        ...buttonStyles,
+                        // Front; Knees
+                        // TopLeft: 330, 1575; BottomRight: 675, 1800  
+                        left: '330px', 
+                        top: '1575px',
+                        width: '345px',
+                        height: '225px',
+                    }}
+                    onClick={() => navigate("/questions/knee/0")}
+                >
+                    Knee
                 </button>
                
+                <button
+                    style={{
+                        ...buttonStyles,
+                        // Front; Ankle
+                        // TopLeft: 400, 2100; BottomRight: 615, 2190  
+                        left: '400px', 
+                        top: '2100px',
+                        width: '215px',
+                        height: '1090px',
+                    }}
+                    onClick={() => navigate("/questions/ankle/0")}
+                >
+                    Ankle
+                </button>
+
+                <button
+                    style={{
+                        ...buttonStyles,
+                        // Front; Feet
+                        // TopLeft: 350, 2190; BottomRight: 675, 2315  
+                        left: '350px', 
+                        top: '2190px',
+                        width: '325px',
+                        height: '125px',
+                    }}
+                    onClick={() => navigate("/questions/feet/0")}
+                >
+                    Feet
+                </button>
+
+                <button
+                    style={{
+                        ...buttonStyles,
+                        // Front; Neck
+                        // TopLeft: 425, 425; BottomRight: 585, 480  
+                        left: '425px', 
+                        top: '425px',
+                        width: '130px',
+                        height: '55px',
+                    }}
+                    onClick={() => navigate("/questions/neck/0")}
+                >
+                    Neck
+                </button>
+
+                <button
+                    style={{
+                        ...buttonStyles,
+                        // Front; Back
+                        // TopLeft: 375, 480; BottomRight: 650, 900  
+                        left: '375px', 
+                        top: '480px',
+                        width: '275px',
+                        height: '420px',
+                    }}
+                    onClick={() => navigate("/questions/Back/0")}
+                >
+                    Back
+                </button>
             </div>
 
 
-            {/*
+            {
             <div className="flex flex-row flex-wrap gap-y-[70px] gap-x-[50px] justify-center">
                 <div className="flex flex-col gap-y-[20px] items-center min-w-[100px]">
                     <Button color="blue" handleClick={() => navigate("/questions/hand/0")}>
@@ -329,7 +297,7 @@ export function BodyPartSelection() {
                         Foot
                     </Button>
                 </div>
-            </div> */}
+            </div> }
         </>
     )
 }
